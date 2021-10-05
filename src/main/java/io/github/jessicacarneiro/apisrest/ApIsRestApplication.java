8d48ddca1be9c89dd63b10f64e5fc03c45c98d22
Jéssica Carneiro <jessica.carneiro@gympass.com>
2021-10-05 14:26:09 -0300

    Add initial files

---
+++ src/main/java/io/github/jessicacarneiro/apisrest/ApIsRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.jessicacarneiro.apisrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIsRestApplication.class, args);
+	}
+
+}
