9e067e1a3443b1828679bad91f3ba00812a70290
Jéssica Carneiro <jessica.carneiro@gympass.com>
2021-11-10 15:37:32 -0300

    Ignore main class and auto generated methods from code coverage

--- src/main/java/io/github/jessicacarneiro/apisrest/ApisRestApplication.java
+++ src/main/java/io/github/jessicacarneiro/apisrest/ApisRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApIsRestApplication {
+public class ApisRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApIsRestApplication.class, args);
+		SpringApplication.run(ApisRestApplication.class, args);
 	}
 
 }
