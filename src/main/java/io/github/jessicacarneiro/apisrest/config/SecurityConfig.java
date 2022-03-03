92f9ef341f76d37d04774f4834b519a7617b555f
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2022-03-03 11:53:47 +0000

    Add setup for in-memory users

--- src/main/java/io/github/jessicacarneiro/apisrest/config/SecurityConfig.java
+++ src/main/java/io/github/jessicacarneiro/apisrest/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package io.github.jessicacarneiro.apisrest.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,5 +30,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String password = "{noop}password";
+
+        User.UserBuilder driver = User.builder()
+                .username("driver")
+                .password(password)
+                .roles("DRIVER");
+
+        User.UserBuilder passenger = User.builder()
+                .username("passenger")
+                .password(password)
+                .roles("PASSENGER");
+
+        User.UserBuilder admin = User.builder()
+                .username("admin")
+                .password(password)
+                .roles("ADMIN");
+
+        auth.inMemoryAuthentication()
+                .withUser(driver)
+                .withUser(passenger)
+                .withUser(admin);
     }
 }
