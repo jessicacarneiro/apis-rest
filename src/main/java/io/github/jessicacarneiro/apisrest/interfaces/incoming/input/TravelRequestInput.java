a0d59dc700571b1eed07edcb27e8bb8832613215
Jéssica Carneiro <jessicaccarneiro91@gmail.com>
2022-03-17 15:25:10 +0000

    Handle origin and destination empty exceptions

--- src/main/java/io/github/jessicacarneiro/apisrest/interfaces/incoming/input/TravelRequestInput.java
+++ src/main/java/io/github/jessicacarneiro/apisrest/interfaces/incoming/input/TravelRequestInput.java
@@ -1,13 +1,18 @@
 package io.github.jessicacarneiro.apisrest.interfaces.incoming.input;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TravelRequestInput {
 
-    @NotNull(message = "passengerId should not be null")
+    @NotNull(message = "Field passengerId should not be null")
     Long passengerId;
+
+    @NotEmpty(message = "Field origin should should not be empty")
     String origin;
+
+    @NotEmpty(message = "Field destination should should not be empty")
     String destination;
 }
